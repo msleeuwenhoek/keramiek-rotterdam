bc36be7ba936db5923fae5223a28e96d5df6cf18
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-04-07 21:51:02 +0200

    homepage css outline

--- src/CeramicsSections.js
+++ src/CeramicsSections.js
@@ -6,43 +6,53 @@ export default function CeramicsSections() {
     <div className="CeramicsSections">
       <section className="split">
         <img src={require("./images/pottery-wheel.png")} />
-        <h4>Follow a pottery course</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
-        </p>
+        <div className="img-description">
+          <h4>Follow a pottery course</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+        </div>
       </section>
       <section className="split">
+        <div className="img-description">
+          <h4>Follow a pottery course</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+        </div>{" "}
         <img src={require("./images/pottery-oven.png")} />
-        <h4>Follow a pottery course</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
-        </p>
       </section>
       <section className="split">
         <img src={require("./images/pottery-glazing.png")} />
-        <h4>Follow a pottery course</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
-        </p>
+        <div className="img-description">
+          <h4>Follow a pottery course</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+        </div>
       </section>
       <section className="split">
+        <div className="img-description">
+          <h4>Follow a pottery course</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+        </div>{" "}
         <img src={require("./images/pottery-oven-2.png")} />
-        <h4>Follow a pottery course</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
-        </p>
       </section>
       <section className="split">
         <img src={require("./images/pottery-usage.png")} />
-        <h4>Follow a pottery course</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
-        </p>
+        <div className="img-description">
+          <h4>Follow a pottery course</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+        </div>
       </section>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,11 @@ import CeramicsSections from "./CeramicsSections";
 export default function Header() {
   return (
     <div className="Home">
-      <section className="split">
+      <div className="header">
         <h1>Keramiek Rotterdam</h1>
         <h2>Hester van den Haak</h2>
         <button>Join</button>
-      </section>
+      </div>
       <CeramicsSections />
     </div>
   );
