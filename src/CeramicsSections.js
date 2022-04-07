4d09540ef2b145879bef1ad8f2432374b26b57b9
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-04-07 21:34:31 +0200

    fixed images by removing white space

--- src/CeramicsSections.js
+++ src/CeramicsSections.js
@@ -1,8 +1,17 @@
 import React from "react";
+import "./CeramicsSections.css";
 
 export default function CeramicsSections() {
   return (
     <div className="CeramicsSections">
+      <section className="split">
+        <img src={require("./images/pottery-wheel.png")} />
+        <h4>Follow a pottery course</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+        </p>
+      </section>
       <section className="split">
         <img src={require("./images/pottery-oven.png")} />
         <h4>Follow a pottery course</h4>
