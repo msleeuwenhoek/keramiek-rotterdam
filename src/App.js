a991b9a3779271794b8fd9c3aabbe2409976ba2a
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-04-08 18:56:42 +0200

    styled ceramics sections

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className="container">
-        <Home />
-      </div>
+      <Home />
     </div>
   );
 }
--- src/CeramicsSections.js
+++ src/CeramicsSections.js
@@ -7,19 +7,19 @@ export default function CeramicsSections() {
       <section className="split">
         <img src={require("./images/pottery-wheel.png")} />
         <div className="img-description">
-          <h4>Follow a pottery course</h4>
+          <h4>Create your own cups and bowls</h4>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+            On the manual pottery wheel you will sit down to create your own
+            ceramics. Starting with small cups and bowl, you can move on to
+            other shapes as your skills progress.
           </p>
         </div>
       </section>
       <section className="split">
         <div className="img-description">
-          <h4>Follow a pottery course</h4>
+          <h4>Your work will be baked for you</h4>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+            In between lessons your ceramics will be placed in a ceramics oven.
           </p>
         </div>{" "}
         <img src={require("./images/pottery-oven.png")} />
@@ -27,10 +27,12 @@ export default function CeramicsSections() {
       <section className="split">
         <img src={require("./images/pottery-glazing.png")} />
         <div className="img-description">
-          <h4>Follow a pottery course</h4>
+          <h4>Glaze and decorate your ceramics to your liking.</h4>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+            A range of colours is available for you to dip your ceramics into.
+            You will learn the proper techniques to glaze your work in full. In
+            addition you can paint small details and decorations onto your
+            ceramics.
           </p>
         </div>
       </section>
@@ -47,10 +49,11 @@ export default function CeramicsSections() {
       <section className="split">
         <img src={require("./images/pottery-usage.png")} />
         <div className="img-description">
-          <h4>Follow a pottery course</h4>
+          <h4>Enjoy your creations at home!</h4>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+            At the end of the course you will have your own ceramics to use
+            around your house. Put some houseplants in them or use them for your
+            morning coffee! Everything is dishwasher safe.
           </p>
         </div>
       </section>
